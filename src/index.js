1774c07cdc0ca1b071cf5488ff5d440711de96cd
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-01-23 15:05:59 -0300

    Add nes.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "nes.css/css/nes.min.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
