3d1acc4a1a729f796e65896e01dc5d7e8f869b68
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-01-23 12:41:02 -0300

    Add wouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Link, Route } from "wouter";
 import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <img src={"logo.png"} alt="giffy logo" className="App-logo" aria-label="logo"></img>
+      <Link to="/">
+        <img src={"logo.png"} alt="giffy logo" className="App-logo" aria-label="logo"></img>
+      </Link>
       <Home />
+
+      <Route component={Home} path="/" />
     </div>
   );
 }
