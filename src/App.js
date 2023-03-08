c16210bbad9fe0676da0b221331e605418413121
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-03-08 11:16:27 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Link, Route } from "wouter";
-import "./App.css";
 import { GifsContextProvider } from "./context/GifsContexts";
+import "./App.css";
 import StaticContext from "./context/StaticContext";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import SearchResults from "./pages/SearchResults";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,16 +14,19 @@ function App() {
         name: "Sofia"
       }}>
       <div className="App">
-        <Link to="/">
-          <img src={"logo.png"} alt="giffy logo" className="App-logo" aria-label="logo"></img>
-        </Link>
+        <div className="App-content">
+          <Link to="/">
+            <h1 className="App-logo">GIFFY</h1>
+          </Link>
 
-        <GifsContextProvider>
-          <Route component={Home} path="/" /> 
-          <Route component={SearchResults} path="/search/:keyword/:rating?" />
-          <Route component={Detail} path="/gif/:id" />
-          <Route component={() => <h1 className="nes-text is-error">404 ERROR :(</h1>} path="/404" />
-        </GifsContextProvider>
+          <GifsContextProvider>
+            <Route component={Home} path="/" /> 
+            <Route component={SearchResults} path="/search/:keyword/:rating?" />
+            <Route component={Detail} path="/gif/:id" />
+            <Route component={() => <h1 className="nes-text is-error">404 ERROR :(</h1>} path="/404" />
+          </GifsContextProvider>
+        </div>
+        <Footer />
       </div>
     </StaticContext.Provider>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <span>Code by Sofia Sosa</span>
+      <i className="nes-icon github is-small"></i>
+    </footer>
+  );
+}
