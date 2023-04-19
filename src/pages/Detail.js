caab975fcaa6e3a004622d58c18c8ce4fbe4225a
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-04-19 14:13:11 -0300

    Fix loading spinner

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -7,14 +7,7 @@ import useSingleGif from "hooks/useSingleGif";
 export default function Detail({ params }) {
   const { gif, isLoading, isError } = useSingleGif({ id: params.id });
 
-  if (isLoading) {
-    return (
-      <>
-        <Spinner />
-      </>
-    );
-  }
-
+  if (isLoading) return <Spinner />;
   if (isError) return <Redirect to="/404" />;
   if (!gif) return null;
 
